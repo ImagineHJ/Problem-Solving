1eff1d60c6ce6dc37b32252b2916374364f95f43
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-01-30 10:54:06 +0000

    Update 10816.cpp

--- BOJ/10816.cpp
+++ BOJ/10816.cpp
@@ -1,29 +1,60 @@
+//
+//  main.cpp
+//
+//  Created by JinnyLim on 2023/01/30.
+//
+
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define INF 1e18
 using  namespace std;
+vector<int> cards;
+vector<pair<int, int>> card_num;
+
+int binary_search(int x){
+    int start, end, mid;
+    start = 0; end = card_num.size()-1;
+    
+    while(start<=end){
+        mid = (start+end)/2;
+        if(x==card_num[mid].first) return mid;
+        else if(x<card_num[mid].first) end = mid-1;
+        else start = mid+1;
+    }
+    return -1;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL); //faster
-    int plus[1001]={0, };
-    int minus[1001]={0, };
     
-    int n, m;
+    int n, m, a, idx, cnt=0;
+    
     cin>>n;
     for(int i=0; i<n; i++){
-        int a;
         cin>>a;
-        
-        if(a>=0) plus[a]++;
-        else minus[-a]++;
+        cards.push_back(a);
+    }
+    sort(cards.begin(), cards.end());
+    int last = -1;
+    for(int i=0; i<n; i++){ // count the quantity of each number
+        if(i==0 || cards[i] != cards[i-1]) {
+            card_num.push_back({cards[i], 1});
+            last ++;
+        }
+        else card_num[last].second++;
     }
     cin>>m;
     for(int i=0; i<m; i++){
-        int b;
-        cin>>b;
-        if(b>=0) cout<<plus[b]<<' ';
-        else cout<<minus[-b]<<' ';
+        cin>>a;
+        idx = binary_search(a);
+        if (idx == -1) cnt = 0;
+        else cnt = card_num[idx].second;
+        cout<<cnt<<" ";
     }
-
     
     return 0;
 }
+
