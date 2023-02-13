df3cd4938491d8618cd25e18e454193014f98867
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-02-13 06:15:08 +0000

    Create 2_Add_Two_Numbers.cpp

---
+++ Leet-Code/2_Add_Two_Numbers.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *p1 = l1, *p2=l2, *temp = l1, *pre_temp, *node;
+        int num=0, over=0;
+        while( p1!=NULL || p2!=NULL){
+            pre_temp = temp;
+            temp = p1;
+            if(p1==NULL){
+                temp = p2; // reuse list 2
+                num=p2->val+over;
+                over = num/10;
+                num = num%10;
+                p2 = p2->next; 
+                pre_temp->next = temp; // connect answer list
+            }
+            else if(p2==NULL){
+                num=p1->val+over;
+                over = num/10;
+                num = num%10;
+                p1 = p1->next;
+            }
+            else{
+                num=p1->val+p2->val+over;
+                over = num/10;
+                num = num%10;
+                p1 = p1->next; p2 = p2->next;
+            }
+            temp->val = num; // reuse list 1
+        }
+        if(over==1){ // if one more digit left
+            node = new ListNode(1);
+            temp->next = node;
+        }
+        return l1;
+    }
+};
