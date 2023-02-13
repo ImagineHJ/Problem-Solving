4879de93bdcdfcf748e578839b4a35ea21670db7
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-02-13 12:03:52 +0000

    Update 242_Valid_Anagram.cpp

--- Leet-Code/242_Valid_Anagram.cpp
+++ Leet-Code/242_Valid_Anagram.cpp
@@ -21,19 +21,9 @@ public:
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char, int >map1, map2;
-
-        if(s.size()!=t.size()) return false; // if the length is different
-        // put all in hash map
-        for(int i=0; i<s.size(); i++){
-            map1[s[i]]++;
-        }
-        for(int i=0; i<t.size(); i++){
-            map2[t[i]]++;
-        }
-        for(auto a:map1){
-            if(map2[a.first]!=a.second) return false; // num of alphabet is diff
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s==t) return true;
+        else return false;
     }
 };
