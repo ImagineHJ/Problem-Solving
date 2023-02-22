0ce58b082b03a19a87897306fc0169b981041776
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-02-22 07:06:09 +0000

    Update 236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp

--- Leet-Code/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ Leet-Code/236_Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -1,3 +1,5 @@
+
+// using DFS
 class Solution {
 public:
     bool found = false, p_found=false, q_found=false;
@@ -24,5 +26,31 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         dfs(root, p, q);
         return ans;
+    }
+};
+
+// iterative solution
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        stack<pair<TreeNode*, bool>> st;
+        unordered_map<int, int> um;
+        bool p_found=false, q_found=false;
+        TreeNode *node;
+
+        st.push({root,(!p_found && !q_found)});
+        while(1){
+            node = st.top().first;
+            if(node==p) p_found = true;
+            if(node==q) q_found = true;
+            if(node->left && um[node->left->val]!=1) st.push({node->left, (!p_found && !q_found)});
+            else if(node->right && um[node->right->val]!=1) st.push({node->right, (!p_found && !q_found)});
+            else{
+                if(st.top().second && q_found && p_found) break;
+                um[node->val] = 1;
+                st.pop();
+            }
+        }
+        return st.top().first;
     }
 };
