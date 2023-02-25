7b94d149ed6d2c8c0891ec8c44efcd8216e75106
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-02-25 09:00:37 +0000

    Update 2263.cpp

--- BOJ/2263.cpp
+++ BOJ/2263.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
-#define MAX 100000
-
+#include <vector>
+#include <algorithm>
+#define INF 1e9
 using namespace std;
 
-void preorder(int r, int* left, int* right){
-    cout<<r<<' ';
-    if(left[r]!=0) preorder(left[r], left, right);
-    if(right[r]!=0) preorder(right[r], left, right);
+vector<int> inorder, postorder;
+int preorder[100000];
+int n;
+
+void find_preorder(int post_start, int post_end, int in_start, int in_end, int n){
+    int root, left_size;
+    if(post_start>post_end || in_start>in_end) return;
+    
+    root = postorder[post_end]; // find root
+    preorder[n] = root;
+    left_size = find(inorder.begin(), inorder.end(), root) - inorder.begin()-in_start;
+    
+    // left tree
+    find_preorder(post_start, post_start+left_size-1, in_start, in_start+left_size-1, n+1);
+    // right tree
+    find_preorder(post_start+left_size, post_end-1, in_start+left_size+1, in_end, n+left_size+1);
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL); //faster
     
-        int n; cin>>n;
-        
-        int post[MAX+1]; int in_idx[MAX+1];
-        int left[MAX+1]={0, }; int right[MAX+1]={0, };
-        
-        for(int i=0; i<n; i++){
-            int a; cin>>a;
-            in_idx[a]=i;
-        }
-        for(int i=0; i<n; i++){
-            int a; cin>>a;
-            post[i]=a;
-        }
-
-        int root=0;
-        for(int i=n-1; i>=0; i--){
-            if(i==n-1) {root=post[i]; continue;}
-            
-            int point=root, node=post[i];
-            while(true){
-                if(in_idx[point]>in_idx[node]){ //left child
-                    if(left[point]!=0)
-                        point=left[point];
-                    else {left[point]=node; break;} //왼쪽 자녀가 없는 경우
-                }
-                else{ //right child
-                    if(right[point]!=0)
-                        point=right[point];
-                    else {right[point]=node; break;} //오른쪽 자녀가 없는 경우
-                }
-            }//while
-        }
-        
-        preorder(root, left, right);
-        cout<<'\n';
+    int a;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>a;
+        inorder.push_back(a);
+    }
+    for(int i=0; i<n; i++){
+        cin>>a;
+        postorder.push_back(a);
+    }
+    
+    find_preorder(0, n-1, 0, n-1, 0);
+    for(int i=0; i<n; i++){
+        cout<<preorder[i]<<" ";
+    }
     
     return 0;
 }
