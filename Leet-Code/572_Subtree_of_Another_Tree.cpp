47d500635cfd926a9a01664769c687b2403d45ba
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2023-03-01 05:45:01 +0000

    Update 572_Subtree_of_Another_Tree.cpp

--- Leet-Code/572_Subtree_of_Another_Tree.cpp
+++ Leet-Code/572_Subtree_of_Another_Tree.cpp
@@ -1,3 +1,5 @@
+
+// 1. using recursion to check if the tree is same
 class Solution {
 public:
     bool isSame(TreeNode* root1, TreeNode* root2){
@@ -17,5 +19,49 @@ public:
             if(now->right) q.push(now->right);
         }
         return false;
+    }
+};
+
+// 2. make an postorder list and compare
+ #define MAX 10001
+class Solution {
+public:
+	bool found = false;
+    vector<int> rootPostorder, subRootPostorder, same_index;
+    void makeSubPostorder(TreeNode* root){
+        int value = root? root->val : MAX;
+        if (root){
+            makeSubPostorder(root->left);
+            makeSubPostorder(root->right);
+        }
+        subRootPostorder.push_back(value);
+    }
+    void makeRootPostorder(TreeNode* root){
+        int value = root? root->val : MAX;
+
+        if(root) {
+            makeRootPostorder(root->left); if(found) return;
+            makeRootPostorder(root->right); if(found) return;
+        }
+        rootPostorder.push_back(value);
+
+        vector<int>::iterator iter;
+        for (iter = same_index.begin(); iter != same_index.end(); ) {
+            if(subRootPostorder[*iter]==value) {
+                (*iter)++;
+                if(*iter==subRootPostorder.size()) {found = true; break;}
+                iter++;
+            }
+            else iter = same_index.erase(iter);
+        }
+
+        if(subRootPostorder[0]==value) {
+            same_index.push_back(1);
+        }
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+			makeSubPostorder(subRoot);
+			makeRootPostorder(root);
+			return found;
     }
 };
