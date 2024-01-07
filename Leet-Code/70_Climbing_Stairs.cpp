1349be7264e49f961ceb84e2be55d645de82ee58
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2024-01-07 14:31:22 +0900

    Create 70_Climbing_Stairs.cpp

---
+++ Leet-Code/70_Climbing_Stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[46] ;
+
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<(n+1); i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
