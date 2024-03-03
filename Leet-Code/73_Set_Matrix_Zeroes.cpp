eb9bcfa118e6ab6e0cc0eee6f6052c25c3c5aec8
Haejin Lim <57395765+ImagineHJ@users.noreply.github.com>
2024-03-03 14:44:51 +0900

    Update 73_Set_Matrix_Zeroes.cpp

--- Leet-Code/73_Set_Matrix_Zeroes.cpp
+++ Leet-Code/73_Set_Matrix_Zeroes.cpp
@@ -26,3 +26,47 @@ public:
         for(auto c : cols) if(c.second) makeColZero(matrix, c.first);
     }
 };
+
+// Optimized
+class Solution2 {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col_0=1;
+         
+        // find zeros
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[i].size(); j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j==0){
+                        col_0=0;
+                    }
+                    else{
+                        matrix[0][j]=0;
+                    }
+                }
+            }
+        }
+ 
+        // fill zeros (except row 0 and col 0)
+        for(int i=1; i<matrix.size(); i++){
+            for(int j=1; j<matrix[i].size(); j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        // row 0
+        if(matrix[0][0]==0){
+            for(int j=0; j<matrix[0].size(); j++) {
+                matrix[0][j]=0;
+            }
+        }
+        // col 0
+        if(col_0==0){
+            for(int i=0; i<matrix.size(); i++) {
+                matrix[i][0]=0;
+            }
+        }
+    }
+};
